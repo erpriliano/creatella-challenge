e9fc33e4b49956da8cf65ddeadf14d7b1995453b
Erpriliano Abbas <erpriliano@gmail.com>
2021-01-21 11:15:26 +0700

    Add infinite scroll functionality to fetch more products

--- src/home.js
+++ src/home.js
@@ -1,25 +1,80 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import ProductCard from './product-card';
 
 const Home = () => {
     const baseUrl = 'http://localhost:3000';
+    const [sortBy, setSortBy] = useState(null);
+    const [pagination, setPagination] = useState({
+        data: [],
+        page: 1,
+        limit: 20,
+        isLoading: false
+    });
+    const paginationRef = useRef({});
+    paginationRef.current = pagination;
 
-    const [selected, setSelected] = useState(null);
-    const [products, setProducts] = useState([]);
+    const sortRef = useRef({});
+    sortRef.current = sortBy;
 
     useEffect(() => {
-        loadData(selected);
-    }, [selected]);
+        loadData(sortRef.current);
+    }, [sortRef.current]);
 
-    const loadData = (selected) => {
-        fetch(`${baseUrl}/products?_sort=${selected}`)
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+    const loadData = (selectedSortBy) => {
+        fetch(
+            `${baseUrl}/products?_sort=${selectedSortBy}&_page=${pagination.page}&_limit=${pagination.limit}`
+        )
             .then((response) => {
                 return response.json();
             })
             .then((products) => {
-                setProducts(products);
+                setPagination({
+                    ...paginationRef.current,
+                    data: products
+                });
             })
             .catch((error) => console.log(error));
+    };
+
+    const loadMoreData = (selectedSortBy, page) => {
+        setPagination({ ...paginationRef.current, isLoading: true });
+        fetch(
+            `${baseUrl}/products?_sort=${selectedSortBy}&_page=${page}&_limit=${pagination.limit}`
+        )
+            .then((response) => {
+                return response.json();
+            })
+            .then((products) => {
+                setPagination({
+                    ...paginationRef.current,
+                    data: paginationRef.current.data.concat(products),
+                    page: page,
+                    isLoading: false
+                });
+            })
+            .catch((error) => console.log(error));
+    };
+
+    const handleScroll = () => {
+        const scrollTop =
+            (document.documentElement && document.documentElement.scrollTop) ||
+            document.body.scrollTop;
+        const scrollHeight =
+            (document.documentElement &&
+                document.documentElement.scrollHeight) ||
+            document.body.scrollHeight;
+
+        if (
+            scrollTop + window.innerHeight >= scrollHeight &&
+            !paginationRef.current.isLoading
+        ) {
+            loadMoreData(sortRef.current, paginationRef.current.page + 1);
+        }
     };
 
     return (
@@ -31,7 +86,6 @@ const Home = () => {
                 </h1>
             </div>
             {/* End of Header Section */}
-
             <div className="w-full px-6 pt-16 mb-8 leading-relaxed">
                 <p className="text-base">
                     Here you're sure to find a bargain on some of the finest
@@ -54,44 +108,41 @@ const Home = () => {
             </div>
             {/* End of ads section */}
 
-            {console.log('Product length: ', products.length)}
+            {/* Select sort by section */}
+            <div className="w-full flex justify-center items-center my-6">
+                <label className="mr-2 text-lg font-semibold">Sort By : </label>
+                <select
+                    className="w-1/5 px-2 py-1 rounded-md focus:outline-none text-lg"
+                    onChange={(e) => setSortBy(e.target.value)}
+                >
+                    <option value="">-- Select --</option>
+                    <option value="id">ID</option>
+                    <option value="size">Size</option>
+                    <option value="price">Price</option>
+                </select>
+            </div>
+            {/* End of select sort by section */}
 
-            {products.length === 0 ? (
-                <div className="spinner"></div>
+            {/* Display products */}
+            <div className="w-full grid grid-cols-4 gap-4 p-6 py-2">
+                {pagination.data.map((product) => (
+                    <ProductCard
+                        key={product.id}
+                        product={product.face}
+                        size={product.size}
+                        price={product.price}
+                        date={product.date}
+                    />
+                ))}
+            </div>
+            {pagination.isLoading ? (
+                <div className="bg-gray-100 h-32">
+                    <div className="spinner"></div>
+                </div>
             ) : (
-                <>
-                    {/* Sort section */}
-                    <div className="w-full flex justify-center items-center my-6">
-                        <label className="mr-2 text-lg font-semibold">
-                            Sort By :{' '}
-                        </label>
-                        <select
-                            className="w-1/5 px-2 py-1 rounded-md focus:outline-none text-lg"
-                            onChange={(e) => setSelected(e.target.value)}
-                        >
-                            <option value="">-- Select --</option>
-                            <option value="id">ID</option>
-                            <option value="size">Size</option>
-                            <option value="price">Price</option>
-                        </select>
-                    </div>
-                    {/* End of sort section */}
-
-                    {/* Products display section */}
-                    <div className="w-full grid grid-cols-3 gap-4 p-6 py-2">
-                        {products.map((product) => (
-                            <ProductCard
-                                key={product.id}
-                                product={product.face}
-                                size={product.size}
-                                price={product.price}
-                                date={product.date}
-                            />
-                        ))}
-                    </div>
-                    {/* End of Product display section */}
-                </>
+                <>{null}</>
             )}
+            {/* End of display products */}
         </div>
     );
 };
